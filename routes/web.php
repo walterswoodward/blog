410030154b3cd1d4d1500b8f3e96a444cabc6272
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-10 15:38:59 -0700

    Cache posts with expiration of 20min

--- routes/web.php
+++ routes/web.php
@@ -23,27 +23,31 @@ Route::get('/', function () {
 
 // curly braces indicate a wild card value
 Route::get('posts/{post}', function ($id) {
-    $path = __DIR__ . '/../resources/posts/'.$id.'.html';
-
-    if (!file_exists($path)) {
+    if (!file_exists($path = __DIR__ . '/../resources/posts/'.$id.'.html')) {
         // What should happen? Any of these could work:
         // dump and die
         // dd('file does not exist');
     
         // dump, die, and debug
-        ddd('file does not exist');
+        // ddd('file does not exist');
     
         // throw laravel 404
         // abort(404);
     
         // redirect to homepage
-        // return redirect('/');
+        return redirect('/');
     }
 
-    $post = file_get_contents($path);
-    return view('post', [
-        'post' => $post
-    ]);
+    // cache for the number of seconds passed as second argument
+    $post = cache()->remember("posts.{$id}", 1200, function () use ($path) {
+        var_dump('cache used');
+        return file_get_contents($path); 
+    });
+
+    // use arrow functions if you can (php >= 7)
+    // $post = cache()->remember("posts.{$id}", 1200, fn() => file_get_contents($path));
+
+    return view('post', ['post' => $post]);
 })->whereNumber('post');
 // The above is a shorthand for below. See more here: https://laravel.com/api/8.x/Illuminate/Routing/Route.html
 // })->where('post', '[0-9]+');
