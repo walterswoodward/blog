cf23d75df3f348ae6e40ab204138a3f3ae6b7f6d
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-10 13:17:49 -0700

    Hardcodes three posts to home page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
+});
+
+Route::get('post', function () {
+    return view('post');
 });
