a5bf7f20c55b55b9573ffa225fad3fb5ab455d36
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-11 14:38:49 -0700

    Creates create_posts_table migration - Updates schema in that migration to reflect desired fields - Creates Post.php eloquent model via artisan - Updates views to access id rather than slug - Updates post routing to use id rather than slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,10 +8,10 @@
         @foreach ($posts as $post)
             <article>
                 <h1>
-                    <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                    <a href="/posts/{{ $post->id }}">{!! $post->title !!}</a>
                 </h1>
                 <div>
-                    <p>{{ $post->excerpt }}</p>
+                    <p>{!! $post->excerpt !!}</p>
                 </div>
             </article>
         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
     return view('posts', ['posts' => Post::all()]);
 });
 
-Route::get('posts/{post}', function ($slug) {
-    return view('post', ['post' => Post::findOrFail($slug)]);
+Route::get('posts/{post}', function ($id) {
+    return view('post', ['post' => Post::findOrFail($id)]);
 });
