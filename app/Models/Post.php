cfdc64d37a324e64419fcb26173c88b61ac20526
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-11 10:36:33 -0700

    Sort all posts view by descending date

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -74,7 +74,8 @@ class Post
                     $document->body(),
                     $document->slug
                 );
-            });
+            })
+            ->sortByDesc('date');
     }
     
     public static function find($slug)
