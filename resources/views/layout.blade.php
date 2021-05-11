676aa7a44c98d13ef3fabec11a520d0de6e9ef4f
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-11 11:31:31 -0700

    Utilize blade layout for posts view

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <header>
+        @yield('banner')
+    </header>
+    @yield('content')
+</body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,18 @@
-<!doctype html>
-<head>
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <?php foreach ($posts as $post) : ?>
+@extends ('layout')
+
+@section('banner')
+<h1>My Blog</h1>
+@endsection
+
+@section('content')
+    @foreach ($posts as $post)
         <article>
-            <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
-            <p>{{ $post->excerpt }}</p>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+            </h1>
+            <div>
+                <p>{{ $post->excerpt }}</p>
+            </div>
         </article>
-    <?php endforeach; ?>
-</body>
+    @endforeach
+@endsection
